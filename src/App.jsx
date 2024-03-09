091b5d9fea782dbdeaf58b2680a07458ae1342ce
Debasis Mishra <debasismishra000123@gmail.com>
2024-03-10 00:48:52 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,23 @@ import { useState } from 'react'
 import WelcomeMeaasge from './Components/WelcomeMessage'
 
 function App() {
-  const initialtask=[
-    {name:"Take Milk", date:"30.10.23"},
-    {name:"Go to College", date:"31.10.23"},
-    {name:"Listen Music", date:"now"}
-  ];
-  
-  let [task,settask]=useState(initialtask);
+
+  let [task,settask]=useState([]);
   const updatedtask=(itemname,itemdate)=>{
-    console.log(itemname+" "+itemdate);
-    const newtasks=[...initialtask,{name:itemname, date:itemdate}];
+    const newtasks=[...task,{name:itemname, date:itemdate}];
     settask(newtasks);
   };
+
+  function Handeldelete(deleteditem){
+    const newtodotask=task.filter(item=>item.name !==deleteditem );
+    settask(newtodotask);
+  }
   return (
     <Container>
       <TodoHeading></TodoHeading>
       <AddTodo updatedtask={updatedtask}></AddTodo>
       {task.length=== 0 && <WelcomeMeaasge></WelcomeMeaasge>}
-      <AddTasks Tasks={task}></AddTasks>
+      <AddTasks Tasks={task}  Handeldelete={Handeldelete}></AddTasks>
     </Container>
   )
 
